daac1dea91712355225546c1548665f6c5bf7dc5
Rogio <jjinyeonnok@gmail.com>
2024-10-31 11:05:33 +0900

    디자인 수정

--- main.py
+++ main.py
@@ -20,7 +20,31 @@ st.title('이번주 당첨번호')
 
 st.title(str(최근회차))
 
-st.title(str(lotto_instance.check_num(최근회차)))
+# st.title(str(lotto_instance.check_num(최근회차)))
+
+# HTML 및 CSS 스타일
+st.markdown("""
+<style>
+    .lotto-ball {
+        display: inline-block;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-color: #f9c74f;  /* 공의 색상 */
+        color: white;
+        text-align: center;
+        line-height: 50px;  /* 수직 정렬 */
+        font-size: 20px;
+        margin: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    }
+</style>
+""", unsafe_allow_html=True)
+
+# 로또 번호 표시
+st.write("로또 번호:")
+for key, value in 최근회차.items():
+    st.markdown(f'<div class="lotto-ball">{value}</div>', unsafe_allow_html=True)
 
 # 빈도 = lotto_instance.빈도추출(최근회차-5,최근회차)
 
