e6fb08b015636593271ea8d922e082fb4519a13b
Rogio <jjinyeonnok@gmail.com>
2024-10-31 11:15:28 +0900

    디자인 수정

--- main.py
+++ main.py
@@ -18,9 +18,14 @@ st.title('이번주 당첨번호')
 
 최근회차 = lotto_instance.최근회차()
 
-st.title(str(최근회차))
+st.title(f'회차 : {최근회차}')
+st.title(f'당첨번호 : ')
 
 당첨번호 = lotto_instance.check_num(최근회차)
+
+
+
+# HTML 및 CSS 스타일
 st.markdown("""
 <style>
     .lotto-ball {
@@ -34,6 +39,11 @@ st.markdown("""
         font-size: 20px;
         margin: 10px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    }
+    .lotto-container {
+        display: flex;
+        justify-content: center; /* 가운데 정렬 */
+        flex-wrap: wrap; /* 여러 줄로 표시 가능 */
     }
 </style>
 """, unsafe_allow_html=True)
@@ -54,11 +64,11 @@ def get_color(number):
         return "#000000"  # 기본 색상
 
 # 로또 번호 표시
-st.write("로또 번호:")
+st.markdown('<div class="lotto-container">', unsafe_allow_html=True)
 for value in 당첨번호.values():
     color = get_color(value)
     st.markdown(f'<div class="lotto-ball" style="background-color: {color};">{value}</div>', unsafe_allow_html=True)
-
+st.markdown('</div>', unsafe_allow_html=True)
 # 빈도 = lotto_instance.빈도추출(최근회차-5,최근회차)
 
 
