c68cf774d8efa73111df0d2a1622b24c4bd0aa79
Rogio <jjinyeonnok@gmail.com>
2024-11-04 10:10:06 +0900

    re start

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # main.py
 import streamlit as st
 from page3.get_data import Lotto_class
-from my_html import tab1, tab2
+from my_html import tab1, tab2  # tab1과 tab2 모듈을 임포트
+from tab1 import display_current_numbers  # display_current_numbers 함수를 임포트
+from tab2 import display_past_records  # display_past_records 함수도 임포트
 
 # Lotto_class의 인스턴스 생성
 lotto_instance = Lotto_class()
@@ -38,8 +40,9 @@ st.markdown("""
 tab1, tab2 = st.tabs(["현재 당첨 번호", "과거 당첨 기록"])
 
 with tab1:
+    # 임포트한 `display_current_numbers` 함수를 호출
     display_current_numbers(lotto_instance)
 
-# with tab2:
-#     tab2.display_past_records(lotto_instance, 최근회차)
-
+with tab2:
+    # 임포트한 `display_past_records` 함수를 호출
+    display_past_records(lotto_instance, 최근회차)
